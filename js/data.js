e82463782512940bb0d9d743a3f5992a0949cebf
Jolie Roiseux <jo.roiseux@gmail.com>
2018-12-02 22:59:34 -0800

    add data

---
+++ js/data.js
@@ -0,0 +1,49 @@
+let dogTreats = "<ul><li>Popcorn</li><li>Bones</li><li>Peanut Butter</li><li>Cheese</li></ul>";
+let catTreats = "<ul><li>Temptations</li><li>Tuna</li><li>Milk</li><li>Salmon</li></ul>";
+let otherTreats = "<ul><li>Carrots</li><li>Sunflower Seeds</li><li>Broccoli</li><li>Crickets</li></ul>";
+
+let dogNames = ["Ivy", "Arya", "Gary", "Rufus"];
+let catNames = ["Lily", "Hugo", "Simba", "Milo"];
+let otherNames = ["Mango", "Coco", "Purin", "Bob"];
+
+//I am not sure how to make this into a list. I tried putting it in quotations but it didn't work...
+let layoutList = <div id="animal1" class="animalbox">
+                    <div id="image1" class="image">
+                      <img src="https://dummyimage.com/200x250/000/fff.png" alt="dog">
+                    </div>
+                    <div class="infodiv">
+                      <h3 id="name1"></h3>
+                      <h4 class="treatheader">Favourite Treat</h4>
+                      <p id="bio1"></p>
+                    </div>
+                  </div>
+                  <div id="animal2" class="animalbox">
+                    <div id="image2" class="image">
+                      <img src="https://dummyimage.com/200x250/000/fff.png" alt="dog">
+                    </div>
+                    <div class="infodiv">
+                      <h3 id="name2"></h3>
+                      <h4 class="treatheader">Favourite Treat</h4>
+                      <p id="bio2"></p>
+                    </div>
+                  </div>
+                  <div id="animal3" class="animalbox">
+                    <div id="image3" class="image">
+                      <img src="https://dummyimage.com/200x250/000/fff.png" alt="dog">
+                    </div>
+                    <div class="infodiv">
+                      <h3 id="name3"></h3>
+                      <h4 class="treatheader">Favourite Treat</h4>
+                      <p id="bio3"></p>
+                    </div>
+                  </div>
+                  <div id="animal4" class="animalbox">
+                    <div id="image4" class="image">
+                      <img src="https://dummyimage.com/200x250/000/fff.png" alt="dog">
+                    </div>
+                    <div class="infodiv">
+                      <h3 id="name4"></h3>
+                      <h4 class="treatheader">Favourite Treat</h4>
+                      <p id="bio4"></p>
+                    </div>
+                  </div>
